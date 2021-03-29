560471c56f72e72ff4fbf3b4f5a4cda225604a0b
Lucas Costa <lucas.costa@cjr.org.br>
2021-03-29 16:17:47 -0300

    feat: add tests and function done until X cases

--- src/romano.cpp
+++ src/romano.cpp
@@ -6,7 +6,97 @@ using namespace std;
 
 
 int RomanToDecimal(char const * roman) {
-    int teste = roman == "I";
-    cout << teste << endl;
-    return 1;
+    int i = 0;
+    int integer = 0;
+    int counterC = 0;
+    int counterX = 0;
+    while(roman[i] != '\0'){
+
+        if(roman[i] == 'M'){
+            if(roman[i - 1] == 'C'){
+                if(roman[i - 2] == 'C'){
+                    return 0;
+                }else {
+                    if(roman[i + 1] == 'M'){
+                        return 0;
+                    }
+                    integer -= 100;
+                }
+            }    
+            integer += 1000;
+        }
+
+        if(roman[i] == 'D') {
+            if(roman[i - 1] == 'C'){
+                if(roman[i - 2] == 'C'){
+                    return 0;
+                } else{
+                    integer -= 100;
+                }
+            } else if(roman[i - 1] == 'D' || roman[i - 1] == 'X' || roman[i - 1] == 'I' || roman[i - 1] == 'L' || roman[i - 1] == 'V'){
+                return 0;
+            }
+            integer += 500;
+        }
+
+        if(roman[i] == 'C' && roman[i + 1] != 'M' && roman[i + 1] != 'D'){
+            if(roman[i - 1] == 'X'){
+                counterC--;
+                if(roman[i - 2] == 'X' || roman[i + 1] == 'X'){
+                    return 0;
+                } else{
+                    if(roman[i + 1] == 'C'){
+                        return 0;
+                    }
+                    integer -= 10; 
+                }
+            }
+            counterC++;
+            if(counterC > 3){
+                return 0;
+            }
+            integer += 100;
+        }
+
+        if(roman[i] == 'L') {
+            if(roman[i - 1] == 'X'){
+                if(roman[i - 2] == 'X'){
+                    return 0;
+                } else{
+                    integer -= 10;
+                }
+            } else if(roman[i - 1] == 'L' || roman[i - 1] == 'I' || roman[i - 1] == 'V'){
+                return 0;
+            }
+            integer += 50;
+        }
+
+        if(roman[i] == 'X' && roman[i + 1] != 'C' && roman[i + 1] != 'L'){
+            if(roman[i - 1] == 'I'){
+                counterX--;
+                if(roman[i - 2] == 'I' || roman[i + 1] == 'I'){
+                    return 0;
+                } else{
+                    if(roman[i + 1] == 'X'){
+                        return 0;
+                    }
+                    integer -= 1;
+
+                }
+            }
+            counterX++;
+            if(counterX > 3) {
+                return 0;
+            }
+            integer += 10;
+        }
+
+        i++;
+    }
+    if(integer > 3000 || i >= 31){
+        return 0;
+    }
+    else{
+        return integer;
+    }
 }
--- src/teste_romano.cpp
+++ src/teste_romano.cpp
@@ -2,12 +2,57 @@
 #include "catch.hpp"
 #include "romano.hpp"
  
-#define SUCCESS 1
 #define FAILURE 0
 
 
 
 
 TEST_CASE("Teste", "teste1") {
-    REQUIRE(RomanToDecimal("I") == 1);  
+    REQUIRE(RomanToDecimal("M") == 1000);
+    REQUIRE(RomanToDecimal("D") == 500);
+    REQUIRE(RomanToDecimal("C") == 100);
+    REQUIRE(RomanToDecimal("L") == 50);
+    REQUIRE(RomanToDecimal("X") == 10);
+
+    REQUIRE(RomanToDecimal("MMM") == 3000);  
+    REQUIRE(RomanToDecimal("CM") == 900);
+    REQUIRE(RomanToDecimal("MCM") == 1900);  
+
+    REQUIRE(RomanToDecimal("CD") == 400);    
+
+    REQUIRE(RomanToDecimal("CMXC") == 990);  
+    REQUIRE(RomanToDecimal("CXC") == 190);
+    REQUIRE(RomanToDecimal("CCCXC") == 390);
+    REQUIRE(RomanToDecimal("CDXC") == 490);
+
+    REQUIRE(RomanToDecimal("XL") == 40);
+    REQUIRE(RomanToDecimal("LX") == 60); 
+}
+
+TEST_CASE("Teste erro", "Erro") {
+    REQUIRE(RomanToDecimal("CCM") == FAILURE);
+    REQUIRE(RomanToDecimal("CMM") == FAILURE);
+    
+    REQUIRE(RomanToDecimal("CCD") == FAILURE);
+    REQUIRE(RomanToDecimal("DD") == FAILURE);
+    REQUIRE(RomanToDecimal("LD") == FAILURE);
+    REQUIRE(RomanToDecimal("XD") == FAILURE);
+    REQUIRE(RomanToDecimal("VD") == FAILURE);
+    REQUIRE(RomanToDecimal("ID") == FAILURE);
+
+    REQUIRE(RomanToDecimal("XXC") == FAILURE);
+    REQUIRE(RomanToDecimal("XCX") == FAILURE);
+    REQUIRE(RomanToDecimal("XCC") == FAILURE);
+    REQUIRE(RomanToDecimal("CCCC") == FAILURE);
+    REQUIRE(RomanToDecimal("CCXCC") == FAILURE);
+    
+    REQUIRE(RomanToDecimal("XXL") == FAILURE);
+    REQUIRE(RomanToDecimal("LL") == FAILURE);
+    REQUIRE(RomanToDecimal("VL") == FAILURE);
+    REQUIRE(RomanToDecimal("IL") == FAILURE);
+
+
+
+    REQUIRE(RomanToDecimal("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII") == FAILURE);
+    REQUIRE(RomanToDecimal("MMMM") == FAILURE);
 }
