9cc68cf99ad8285411f5b7d0ab7c47d00ff6aafc
Lucas Costa <lucas.costa@cjr.org.br>
2021-03-30 16:36:14 -0300

    feat: add final tests

--- src/romano.cpp
+++ src/romano.cpp
@@ -10,18 +10,22 @@ int RomanToDecimal(char const * roman) {
     int integer = 0;
     int counterC = 0;
     int counterX = 0;
+    int counterI = 0;
     while(roman[i] != '\0'){
 
         if(roman[i] == 'M'){
             if(roman[i - 1] == 'C'){
                 if(roman[i - 2] == 'C'){
-                    return 0;
+                    return -1;
                 }else {
                     if(roman[i + 1] == 'M'){
-                        return 0;
+                        return -1;
                     }
                     integer -= 100;
                 }
+            }
+            if(roman[i - 1] == 'D'){
+                return -1;
             }    
             integer += 1000;
         }
@@ -29,12 +33,12 @@ int RomanToDecimal(char const * roman) {
         if(roman[i] == 'D') {
             if(roman[i - 1] == 'C'){
                 if(roman[i - 2] == 'C'){
-                    return 0;
+                    return -1;
                 } else{
                     integer -= 100;
                 }
             } else if(roman[i - 1] == 'D' || roman[i - 1] == 'X' || roman[i - 1] == 'I' || roman[i - 1] == 'L' || roman[i - 1] == 'V'){
-                return 0;
+                return -1;
             }
             integer += 500;
         }
@@ -43,17 +47,20 @@ int RomanToDecimal(char const * roman) {
             if(roman[i - 1] == 'X'){
                 counterC--;
                 if(roman[i - 2] == 'X' || roman[i + 1] == 'X'){
-                    return 0;
+                    return -1;
                 } else{
                     if(roman[i + 1] == 'C'){
-                        return 0;
+                        return -1;
                     }
                     integer -= 10; 
                 }
             }
+            if(roman[i - 1] == 'L'){
+                return -1;
+            }
             counterC++;
             if(counterC > 3){
-                return 0;
+                return -1;
             }
             integer += 100;
         }
@@ -61,12 +68,12 @@ int RomanToDecimal(char const * roman) {
         if(roman[i] == 'L') {
             if(roman[i - 1] == 'X'){
                 if(roman[i - 2] == 'X'){
-                    return 0;
+                    return -1;
                 } else{
                     integer -= 10;
                 }
             } else if(roman[i - 1] == 'L' || roman[i - 1] == 'I' || roman[i - 1] == 'V'){
-                return 0;
+                return -1;
             }
             integer += 50;
         }
@@ -75,26 +82,47 @@ int RomanToDecimal(char const * roman) {
             if(roman[i - 1] == 'I'){
                 counterX--;
                 if(roman[i - 2] == 'I' || roman[i + 1] == 'I'){
-                    return 0;
+                    return -1;
                 } else{
                     if(roman[i + 1] == 'X'){
-                        return 0;
+                        return -1;
                     }
                     integer -= 1;
 
                 }
             }
+            if(roman[i - 1] == 'V'){
+                return -1;
+            }
             counterX++;
             if(counterX > 3) {
-                return 0;
+                return -1;
             }
             integer += 10;
+        }
+
+        if(roman[i] == 'V'){
+            if(roman[i - 1] == 'I'){
+                if(roman[i - 2] == 'I'){
+                    return -1;
+                }
+                integer -= 1;
+            }
+            integer += 5;
+        }
+
+        if(roman[i] == 'I' && roman[i + 1] != 'X'){
+            counterI++;
+            if(counterI > 3) {
+                return -1;
+            }
+            integer++;
         }
 
         i++;
     }
     if(integer > 3000 || i >= 31){
-        return 0;
+        return -1;
     }
     else{
         return integer;
--- src/teste_romano.cpp
+++ src/teste_romano.cpp
@@ -1,18 +1,21 @@
 #define CATCH_CONFIG_MAIN   
 #include "catch.hpp"
 #include "romano.hpp"
+#include <iostream>
  
-#define FAILURE 0
+#define FAILURE -1
 
 
 
 
-TEST_CASE("Teste", "teste1") {
+TEST_CASE("Sucesso", "[sucesso]") {
     REQUIRE(RomanToDecimal("M") == 1000);
     REQUIRE(RomanToDecimal("D") == 500);
     REQUIRE(RomanToDecimal("C") == 100);
     REQUIRE(RomanToDecimal("L") == 50);
     REQUIRE(RomanToDecimal("X") == 10);
+    REQUIRE(RomanToDecimal("I") == 1);
+
 
     REQUIRE(RomanToDecimal("MMM") == 3000);  
     REQUIRE(RomanToDecimal("CM") == 900);
@@ -24,15 +27,20 @@ TEST_CASE("Teste", "teste1") {
     REQUIRE(RomanToDecimal("CXC") == 190);
     REQUIRE(RomanToDecimal("CCCXC") == 390);
     REQUIRE(RomanToDecimal("CDXC") == 490);
+    REQUIRE(RomanToDecimal("XCIX") == 99);
 
     REQUIRE(RomanToDecimal("XL") == 40);
     REQUIRE(RomanToDecimal("LX") == 60); 
+    REQUIRE(RomanToDecimal("XLIX") == 49);
+    REQUIRE(RomanToDecimal("III") == 3);
+    
 }
 
-TEST_CASE("Teste erro", "Erro") {
+TEST_CASE("Falha", "[falha]") {
     REQUIRE(RomanToDecimal("CCM") == FAILURE);
     REQUIRE(RomanToDecimal("CMM") == FAILURE);
     
+
     REQUIRE(RomanToDecimal("CCD") == FAILURE);
     REQUIRE(RomanToDecimal("DD") == FAILURE);
     REQUIRE(RomanToDecimal("LD") == FAILURE);
@@ -40,16 +48,27 @@ TEST_CASE("Teste erro", "Erro") {
     REQUIRE(RomanToDecimal("VD") == FAILURE);
     REQUIRE(RomanToDecimal("ID") == FAILURE);
 
+    
+    REQUIRE(RomanToDecimal("CCCC") == FAILURE);
     REQUIRE(RomanToDecimal("XXC") == FAILURE);
     REQUIRE(RomanToDecimal("XCX") == FAILURE);
     REQUIRE(RomanToDecimal("XCC") == FAILURE);
-    REQUIRE(RomanToDecimal("CCCC") == FAILURE);
     REQUIRE(RomanToDecimal("CCXCC") == FAILURE);
+    REQUIRE(RomanToDecimal("LC") == FAILURE);
     
     REQUIRE(RomanToDecimal("XXL") == FAILURE);
     REQUIRE(RomanToDecimal("LL") == FAILURE);
     REQUIRE(RomanToDecimal("VL") == FAILURE);
     REQUIRE(RomanToDecimal("IL") == FAILURE);
+
+    REQUIRE(RomanToDecimal("XXXX") == FAILURE);
+    REQUIRE(RomanToDecimal("IIX") == FAILURE);
+    REQUIRE(RomanToDecimal("VX") == FAILURE);
+    REQUIRE(RomanToDecimal("VXV") == FAILURE);
+
+    REQUIRE(RomanToDecimal("IIIV") == FAILURE);
+
+    REQUIRE(RomanToDecimal("IIII") == FAILURE);
 
 
 
